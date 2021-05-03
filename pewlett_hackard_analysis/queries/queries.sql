955d6e63d373c76855997914f4aea64796c1823d
Al Ricciardelli <alricciardelli@Als-MacBook-Pro.local>
2021-05-02 19:36:39 -0700

    Queries added

--- pewlett_hackard_analysis/queries/queries.sql
+++ pewlett_hackard_analysis/queries/queries.sql
@@ -47,9 +47,30 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title INT NOT NULL,
+	title varchar NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     PRIMARY KEY (emp_no, title, from_date)
 );
+
+-- Retirement eligibility
+select first_name, last_name
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+select count(first_name)
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Create table from retirement eligibility data
+select first_name, last_name
+into retirement_info
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+select * from retirement_info;
