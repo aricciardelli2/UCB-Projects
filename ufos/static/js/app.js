1b98d8c1e909bfc48b119dc740d0bc33bb45f52c
Al Ricciardelli <alricciardelli@Als-MacBook-Pro.local>
2021-05-30 17:43:48 -0700

    Initial table builder function

---
+++ ufos/static/js/app.js
@@ -0,0 +1,22 @@
+// Import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    // Clear data
+    tbody.html("");
+
+    // Loop through each object in the data
+    data.forEach((dataRow) => {
+        // Append a row to the table body
+        let row = tbody.append("tr");
+        // Loop through each field in the dataRow objec
+        // and add each value as a table cell 
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td")
+            cell.text(val)
+        });
+    });
+}
